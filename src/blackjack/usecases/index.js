24ec49f139efa18afaf36b4ea29dbb974124ab65
Santiago Orozco <everardoorozcocarmona@gmail.com>
2023-01-18 14:34:57 -0500

    subiendo proyecto

---
+++ src/blackjack/usecases/index.js
@@ -0,0 +1,5 @@
+export { crearDeck } from './crear-deck';
+export { pedirCarta } from './pedirCarta'; 
+export { turnoComputadora } from './turno-computadora';
+export { valorCarta } from './valor-carta';
+export { crearCartaHTML } from './crear-carta-html';
